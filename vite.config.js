84f7b11819cd19e7fd2098b658206ec88042b71b
Owais Ahsan <owais.ahsan@hotmail.com>
2024-05-17 18:00:55 +0500

    Config fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/OSTify/frontend/",
+    base: "/OSTify/",
     plugins: [react()],
 });
